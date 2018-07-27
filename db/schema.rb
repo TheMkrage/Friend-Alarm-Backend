28c250c397db98b34e416c555577101a6f51090a
Matthew Krager <metoofarmer@yahoo.com>
2018-07-26 19:05:46 -0700

    user and tests

--- db/schema.rb
+++ db/schema.rb
@@ -21,6 +21,8 @@ ActiveRecord::Schema.define(version: 2018_07_27_002416) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "facebook_connection"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  def setup
+    @invalid_user= users(:invalid)
+    @valid_user= users(:valid)
+  end
+
+  test "invalid long username" do
+    @invalid_user.valid?
+    assert_not_nil @invalid_user.errors[:username]
+  end
+
+  test "invalid without username" do
+    no_username_user = @valid_user
+    no_username_user.username = nil
+    no_username_user.valid?
+    assert_not_nil no_username_user.errors[:username]
+  end
 end
