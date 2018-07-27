5449fa68de8786386561f03c9ee92d3db376c0c3
Matthew Krager <metoofarmer@yahoo.com>
2018-07-27 00:14:33 -0700

    users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  get 'user/show'
+  post 'user/create'
+  get 'user/search'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
