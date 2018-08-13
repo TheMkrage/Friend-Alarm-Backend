6596b75a0dceb8eea08feed5c07a7f04460ace61
Matthew Krager <metoofarmer@yahoo.com>
2018-08-13 12:29:24 -0700

    test config

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,14 @@ module FriendAlarm
     config.load_defaults 5.2
 
     config.api_only = true
+
+    puts '\nHERE YOU GO'
+    puts ENV['aws-access']
+    puts 'HERE YOU GO\n'
+    AWS.config(
+      access_key_id: ENV['aws-access'],
+      secret_access_key: ENV['aws-secret']
+    )
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
