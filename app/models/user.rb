c24c3a34f04e14863a79fc2a5896b1d1a48bd906
Matthew Krager <metoofarmer@yahoo.com>
2018-08-14 13:06:01 -0700

    change plucking method

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ApplicationRecord
 
   def alarms
     made_alarms = Alarm.where(user_id: self.id)
-    referred_alarms = UserAlarm.where(owner_id: self.id).pluck(:alarm)
+    referred_alarms = UserAlarm.where(owner_id: self.id).map {|user_alarm| user_alarm.alarm }.flatten
     alarms = made_alarms + referred_alarms
     return alarms
   end
