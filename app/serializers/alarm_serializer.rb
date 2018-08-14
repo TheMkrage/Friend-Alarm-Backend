572f004ef5b257d66ee1f17ca17d0775931dee6d
Matthew Krager <metoofarmer@yahoo.com>
2018-08-14 11:26:27 -0700

    correct serialization

--- app/serializers/alarm_serializer.rb
+++ app/serializers/alarm_serializer.rb
@@ -1,3 +1,13 @@
 class AlarmSerializer < ActiveModel::Serializer
-  attributes :id
+  attributes :id, :name, :duration, :file_url, :username
+
+  def file_url
+    if self.audio_file.attached?
+      self.audio_file.service_url
+    end
+  end
+
+  def username
+    object.user.username
+  end
 end
