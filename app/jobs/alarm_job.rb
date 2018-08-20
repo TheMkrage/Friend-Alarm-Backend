c0fda97366f6258afad9e33130bef6cc459e1bdd
Matthew Krager <metoofarmer@yahoo.com>
2018-08-19 22:22:21 -0700

    notifications include url to download

--- app/jobs/alarm_job.rb
+++ app/jobs/alarm_job.rb
@@ -7,19 +7,21 @@ class AlarmJob < ApplicationJob
   def perform(user, alarm)
 
     APN.certificate = File.read('apple_push_notification_dev.pem')
-    puts "sent"
-    puts user.apn_token
+
     # Create a notification that alerts a message to the user, plays a sound, and sets the badge on the app
     notification = Houston::Notification.new(device: user.apn_token)
-    notification.alert = 'Hello, World!'
-
+    puts "helo"
+    notification.alert = alarm.name + " from " + alarm.user.username
+    puts user.apn_token
     # Notifications can also change the badge count, have a custom sound, have a category identifier, indicate available Newsstand content, or pass along arbitrary data.
-    notification.badge = 57
-    # notification.sound = 'sosumi.aiff'
-    notification.category = 'INVITE_CATEGORY'
+    notification.category = 'ALARM'
     notification.content_available = true
-    notification.custom_data = { foo: 'bar' }
-
+    if alarm.audio_file.attached?
+      notification.custom_data = { id: alarm.id, url: alarm.audio_file.service_url }
+    else
+      notification.custom_data = { id: alarm.id }
+    end
+    #puts notification.custom_data
     # And... sent! That's all it takes.
     APN.push(notification)
   end
