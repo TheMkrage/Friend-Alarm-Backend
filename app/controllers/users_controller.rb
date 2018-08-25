dea56fa131e58d3741d85b02e51a8b60123edae4
Matthew Krager <metoofarmer@yahoo.com>
2018-08-25 12:29:19 -0700

    debug nil alarm

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -42,7 +42,10 @@ class UsersController < ApplicationController
   def schedule_alarm
     @user.alarm_time = schedule_alarm_params[:time]
     @user.save
+    puts "schedule"
+    puts schedule_alarm_params[:alarm_id]
     alarm = Alarm.find_by(id: schedule_alarm_params[:alarm_id])
+    puts alarm
     AlarmJob.set(wait_until: Time.parse(schedule_alarm_params[:time])).perform_later @user, alarm
     puts "Alarm Set"
     render json: true
